f141c6d2872c4dbabd6d2e0f86b02a79338cf27c
Emmanuelahdamilola <emmanuelahdamilola1992@gmail.com>
2023-10-10 06:08:42 -0700

    modified

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -13,7 +13,7 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 	if (!tree->left && !tree->right)
-		leaves ++;
+		leaves++;
 	leaves += binary_tree_leaves(tree->left);
 	leaves += binary_tree_leaves(tree->right);
 
