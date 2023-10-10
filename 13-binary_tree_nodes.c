aee5c6cd4fc628fc3c9719a9cfe888d71b94a847
Emmanuelahdamilola <emmanuelahdamilola1992@gmail.com>
2023-10-10 06:10:14 -0700

    modified

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -4,7 +4,8 @@
  * binary_tree_nodes - Counts the number of non-leaf nodes in a binary tree.
  * @tree: Pointer to the root node of the binary tree.
  *
- * Return: The number of non-leaf nodes in the binary tree, 0 if the tree is NULL.
+ * Return: The number of non-leaf nodes in the binary tree,
+ * 0 if the tree is NULL.
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
@@ -13,7 +14,7 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 	if (tree->left || tree->right)
-		nodes ++;
+		nodes++;
 	nodes += binary_tree_nodes(tree->left);
 	nodes += binary_tree_nodes(tree->right);
 	return (nodes);
